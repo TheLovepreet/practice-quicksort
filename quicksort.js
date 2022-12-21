c4db575520cec02f865330d4cbbe8d44b1bf3984
TheLove <lovepreetsinghcad@gmail.com>
2022-12-21 12:31:20 -0500

    quick sort and merge sort complete

--- quicksort.js
+++ quicksort.js
@@ -1,5 +1,3 @@
-function quicksort(arr) {
-
   // Check if the input is length 1 or less
     // If so, it's already sorted: return
 
@@ -13,8 +11,20 @@ function quicksort(arr) {
   // Recursively sort the right
 
   // Return the left, pivot and right in sorted order
+  function quicksort(arr){
+  if(arr.length <= 1){
+    return arr;
+  }
+  let leftArr  = [];
+  let rightArr = [];
+  let pivot = arr[arr.length-1];
+  for(let i = 0; i < arr.length-1; i++){
+    arr[i] < pivot ? leftArr.push(arr[i]) : rightArr.push(arr[i]);
+}
+return [...quicksort(leftArr),pivot,...quicksort(rightArr)];
 
 }
 
-
 module.exports = [quicksort];
+
+
